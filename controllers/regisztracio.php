bca6bbaabe0d092ee4763db56d14f5be69b11c3c
TLPLTSzft <tlpltszft@gmail.com>
2022-11-05 19:47:42 +0100

    Regisztráció

---
+++ controllers/regisztracio.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  require_once "models/FelhasznaloModel.php";
+  $felh_model = new FelhasznaloModel();
+  $felhasznalo_nev = $_POST['felhasznalo_nev'];
+  $email = $_POST['email'];
+  $jelszo = $_POST['jelszo'];
+  $teljes_nev = $_POST['teljes_nev'];
+  $szuletesi_datum = $_POST['szuletesi_datum'];
+  $iranyito_szam = $_POST['iranyito_szam'];
+  $varos = $_POST['varos'];
+  $cim = $_POST['cim'];
+  $felh_model->regisztracio($felhasznalo_nev, $email, $jelszo, $teljes_nev, $szuletesi_datum, $iranyito_szam, $varos, $cim);
+}
+include "views/regisztracio_urlap.php";
---
+++ views/regisztracio_urlap.php
@@ -0,0 +1,48 @@
+<h1>Regisztráció</h1>
+<form method="POST">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="mb-3 col-md-6 col-lg-4 col-xl-3 col-xxl-2">
+        <label for="felhasznalo_nev_input" class="form-label">Felhasználó név</label>
+        <input type="text" class="form-control" id="felhasznalo_nev_input" name="felhasznalo_nev">
+      </div>
+
+      <div class="mb-3 col-md-6 col-lg-4 col-xl-3 col-xxl-2">
+        <label for="email_input" class="form-label">Email</label>
+        <input type="text" class="form-control" id="email_input" name="email">
+      </div>
+
+      <div class="mb-3 col-md-6 col-lg-4 col-xl-3 col-xxl-2">
+        <label for="jelszo_input" class="form-label">Jelszó</label>
+        <input type="text" class="form-control" id="jelszo_input" name="jelszo">
+      </div>
+
+      <div class="mb-3 col-md-6 col-lg-4 col-xl-3 col-xxl-2">
+        <label for="teljes_nev_input" class="form-label">Teljes név</label>
+        <input type="text" class="form-control" id="teljes_nev_input" name="teljes_nev">
+      </div>
+
+      <div class="mb-3 col-md-6 col-lg-4 col-xl-3 col-xxl-2">
+        <label for="szuletesi_datum_input" class="form-label">Születési dátum</label>
+        <input type="text" class="form-control" id="szuletesi_datum_input" name="szuletesi_datum">
+      </div>
+
+      <div class="mb-3 col-md-6 col-lg-4 col-xl-3 col-xxl-2">
+        <label for="iranyito_szam_input" class="form-label">Irányító szám</label>
+        <input type="text" class="form-control" id="iranyito_szam_input" name="iranyito_szam">
+      </div>
+
+      <div class="mb-3 col-md-6 col-lg-4 col-xl-3 col-xxl-2">
+        <label for="varos_input" class="form-label">Város</label>
+        <input type="text" class="form-control" id="varos_input" name="varos">
+      </div>
+
+      <div class="mb-3 col-md-6 col-lg-4 col-xl-3 col-xxl-2">
+        <label for="cim_input" class="form-label">Cím</label>
+        <input type="text" class="form-control" id="cim_input" name="cim">
+      </div>
+
+    </div>
+  </div>
+  <button type="submit" class="btn btn-outline-primary">Regisztráció</button>
+</form>
